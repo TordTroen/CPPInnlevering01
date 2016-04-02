6cbfb31eb0d0e2a8d31f5ca7bf1c01d5d62d35dc
tobgos <tobias.g.schultz@gmail.com>
2016-04-02 15:01:06 +0200

    Comment main

--- C++Innlevering01/Main.cpp
+++ C++Innlevering01/Main.cpp
@@ -23,35 +23,36 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	InputManager::GetInstance().Init();
-	Time time;
-	GUIManager gui;
-	BoardManager board;
-	float paddleSpeed = 1000;
-	float ballSpeed = 0.25;
-	int score = 0;
-	GameManager::GetInstance().SetGameState(MainMenu);
+	InputManager::GetInstance().Init();			// Keeps track of keyboard and mouse input
+	Time time;									// Keeps track of ellapsed time between frames
+	GUIManager gui;								// Keeps track of activating visual elements like menu, ball, paddle and boxes
+	BoardManager board;							// Keeps track of the board and it's level.
+	float paddleSpeed = 1000;					// The speed of the paddle
+	float ballSpeed = 0.25;						// The speed of the ball
+	int score = 0;								// The number of boxes that is shot down
 
-	// Initialize stuff that gameobject, gameloop, etc are dependant on
+	GameManager::GetInstance().SetGameState(MainMenu);	
+
+	// Initialize SDL components for background, window, sizes, etc. that gameobject, gameloop, etc are dependant on
 	if (SDLWrapper::GetInstance().InitializeWindow("Breakout", GameManager::GetInstance().GetWindowWidth(),
 		GameManager::GetInstance().GetWindowHeight(), Color(0, 200, 200)) == 0)
 	{
-		// Level test
-		//GameObject* brick = GameObjectManager::GetInstance().CreateObject();
-		//brick->AddComponent(new LevelBrick(Vector2D(100, 100), Yellow, 1, false));
-		Level* level = new Level("3333333333\n2222222222\n1111111111", 90);
+
+		// Holds start position, number of bricks and name of level.
+		// Parameter holds 1 digit for each brick - each number has different coloring, 1: blue, 2: black
+		Level* level = new Level("3333333333\n2222222222\n1111111111", 90); 
 		board.LoadLevel(level);
 
 		gui.SetupMenus();
 
-		// Make the ball
+		// Make the ball object. Both the visual ball and the positioning of the ball. 
 		GameObject* ballObj = GameObjectManager::GetInstance().CreateObject(Tags::Ball);
 		ballObj->AddComponent(new ImageRenderer("WhiteTexture.png", Color(100, 150, 200)));
 		ballObj->AddComponent(new BoxCollider(false));
 		ballObj->AddComponent(new BallMovement(Vector2D(2, 1), ballSpeed));
 		ballObj->GetTransform()->SetRect(Rect(200, 200, 20, 20));
 
-		// Make the paddle
+		// Make the paddle object. Both the visual paddle and the positioning of the paddle. 
 		GameObject* paddleObj = GameObjectManager::GetInstance().CreateObject(Tags::Paddle);
 		paddleObj->AddComponent(new ImageRenderer("WhiteTexture.png", Color(100, 100, 255)));
 		paddleObj->AddComponent(new BoxCollider());
@@ -66,8 +67,9 @@ int main(int argc, char** argv)
 		GameObject* rightWall = GameObjectManager::GetInstance().CreateObject({ new ImageRenderer("WhiteTexture.png"), new BoxCollider() }, Tags::WallRight);
 		GameObject* topWall = GameObjectManager::GetInstance().CreateObject({ new ImageRenderer("WhiteTexture.png"), new BoxCollider() }, Tags::WallTop);
 		GameObject* bottomWall = GameObjectManager::GetInstance().CreateObject({ new ImageRenderer("WhiteTexture.png"), new BoxCollider() }, Tags::WallBottom);
-		float inset = 0;
-		float wallDepth = 25;
+		
+		float inset		= 100;	// Holds the padding between size window and game walls, 0 is no padding - 100 is some padding, etc..			
+		float wallDepth = 1;	// Holds the thickness of the walls
 		float scw = GameManager::GetInstance().GetWindowWidth();
 		float sch = GameManager::GetInstance().GetWindowHeight();
 		leftWall->GetTransform()->SetRect(Rect(inset - wallDepth, 0, wallDepth, sch));
@@ -75,19 +77,21 @@ int main(int argc, char** argv)
 		topWall->GetTransform()->SetRect(Rect(0, inset - wallDepth, scw, wallDepth));
 		bottomWall->GetTransform()->SetRect(Rect(0, sch - inset, scw, wallDepth));
 
-		// Game loop
-		GameState gameState = MainMenu;
+		
+		GameState gameState = MainMenu; // Sets the positioning data. x,y,w,h, position, gravity
+
+		//	The game loop
 		while (gameState != Exit)
 		{
-			gameState = GameManager::GetInstance().GetGameState();
+			gameState = GameManager::GetInstance().GetGameState(); // Holds which state the game is in: MainMenu, Paused, InGame, Exit
 
-			//// Update everything that needs to be updated every frame ////
+			// Update everything that needs to be updated every frame
 			GameObjectManager::GetInstance().Update();
 			InputManager::GetInstance().Update();
 			time.Update();
 			CollisionManager::Update();
 
-			//// Logic ////
+			//	Exit program if user pushes escape
 			if (InputManager::GetInstance().GetKeyDown(SDL_SCANCODE_ESCAPE))
 			{
 				break;
@@ -102,10 +106,6 @@ int main(int argc, char** argv)
 					playerController->ChangeScore(1);
 					gui.UpdateScoreText(playerController->GetScore());
 				}
-				// DEBUG
-
-				//Vector2D velocity = Vector2D(InputManager::GetInstance().GetAxis(SDL_SCANCODE_RIGHT, SDL_SCANCODE_LEFT), InputManager::GetInstance().GetAxis(SDL_SCANCODE_DOWN, SDL_SCANCODE_UP));
-				//paddleObj->GetTransform()->Translate(velocity * paddleSpeed * time.DeltaTime(), true);
 			}
 			
 			//// Render ////
