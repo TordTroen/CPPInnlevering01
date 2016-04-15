dbd2c6d108c4a1685838c546baa32273fefecbe9
andre <andre@DESKTOP-22230M3>
2016-04-15 16:58:31 +0200

    Ball png added, PowerUp bricks falling from destroyed bricks

--- C++Innlevering01/PowerUpBlock.cpp
+++ C++Innlevering01/PowerUpBlock.cpp
@@ -1,14 +1,18 @@
 #include "PowerUpBlock.h"
+#include "LevelBrick.h"
 #include "Vector2D.h"
 #include "InputManager.h"
 #include "GameManager.h"
+#include "GameObjectManager.h"
 #include "GameObject.h"
 #include "Transform.h"
+#include "Player.h"
 #include "Time.h"
+#include "Tags.h"
 
 
-PowerUpBlock::PowerUpBlock(Vector2D start, float speed)
-	: starting(start), speedBox(speed)
+PowerUpBlock::PowerUpBlock(Vector2D start, float speed, PowerType pType)
+	: starting(start), speedBox(speed), powerType(pType)
 {
 }
 
@@ -17,8 +21,36 @@ PowerUpBlock::~PowerUpBlock()
 {
 }
 
+void PowerUpBlock::Awake()
+{
+	player = GameObjectManager::GetInstance().FindGameObjectByTag(Tags::Paddle)->GetComponent<Player>();
+}
+
 void PowerUpBlock::Update()
 {
 	Vector2D velocity = Vector2D(0, 0.5);
 	GetTransform()->Translate(velocity * speedBox * Time::DeltaTime(), true);
+}
+
+void PowerUpBlock::OnCollisionEnter(const Collider* const other)
+{
+	if (other->GetGameObject()->CompareTag(Tags::Paddle))
+	{
+		if (powerType == PowerType::PowerPaddle) 
+		{
+			GetGameObject()->SetActive(false);
+			player->LongPaddle(true);
+			std::cout << "Long paddle fungerer!!" << std::endl;
+		}
+		else if (powerType == PowerType::PowerLife) 
+		{
+			player->SetLifeLeft(player->GetLifeLeft() + 1);
+			std::cout << "Ekstra liv fungerer!!" << std::endl;
+		}
+	}
+	else if (other->GetGameObject()->CompareTag(Tags::WallBottom))
+	{
+		GetGameObject()->SetActive(false);
+		//TODO: Destroy object
+	}
 }
--- C++Innlevering01/PowerUpBlock.h
+++ C++Innlevering01/PowerUpBlock.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "Component.h"
 #include "Rect.h"
+#include "Collider.h"
+#include "LevelBrick.h"
+#include "Player.h"
 class PowerUpBlock :
 	public Component
 {
 public:
-	PowerUpBlock(Vector2D start, float speed);
+	PowerUpBlock(Vector2D start, float speed, PowerType pType);
 	~PowerUpBlock();
+	void Awake() override;
 	void Update() override;
 
+	void OnCollisionEnter(const Collider * const other) override;
+
 private:
+	Player* player;
+	PowerType pType;
 	Vector2D starting;
+	LevelBrick* BlockObj;
+	PowerType powerType;
 	float speedBox;
 };
 
