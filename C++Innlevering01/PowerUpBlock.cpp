393becdf8e2700c530516362fd86e2cabe57b491
andre <andre@DESKTOP-22230M3>
2016-04-15 14:36:16 +0200

    PowerUp Brick falling

--- C++Innlevering01/PowerUpBlock.cpp
+++ C++Innlevering01/PowerUpBlock.cpp
@@ -1,12 +1,24 @@
 #include "PowerUpBlock.h"
+#include "Vector2D.h"
+#include "InputManager.h"
+#include "GameManager.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "Time.h"
 
 
-
-PowerUpBlock::PowerUpBlock()
+PowerUpBlock::PowerUpBlock(Vector2D start, float speed)
+	: starting(start), speedBox(speed)
 {
 }
 
 
 PowerUpBlock::~PowerUpBlock()
 {
+}
+
+void PowerUpBlock::Update()
+{
+	Vector2D velocity = Vector2D(0, 0.5);
+	GetTransform()->Translate(velocity * speedBox * Time::DeltaTime(), true);
 }
--- C++Innlevering01/PowerUpBlock.h
+++ C++Innlevering01/PowerUpBlock.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "Component.h"
+#include "Rect.h"
 class PowerUpBlock :
 	public Component
 {
 public:
-	PowerUpBlock();
+	PowerUpBlock(Vector2D start, float speed);
 	~PowerUpBlock();
+	void Update() override;
+
+private:
+	Vector2D starting;
+	float speedBox;
 };
 
